1b597a9da4d921dac3e1a81ca2add4d84b088bb4
macs1756 <kindrativmaksum312@gmail.com>
2023-12-31 16:17:18 +0200

    v2

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Working...</Text>
+      
+      <Image style={styles.imagePrimary} source={require('./assets/initial-photo.jpg')} />
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +17,16 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    position: 'relative'
   },
+
+  imagePrimary: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    resizeMode: 'cover',
+    height: '100%',
+    width: '100%',
+  }
 });
+
