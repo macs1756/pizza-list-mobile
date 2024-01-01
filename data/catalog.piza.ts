e9ef0e43d205c2bd75f2ec6030b98629017b56a2
macs1756 <kindrativmaksum312@gmail.com>
2024-01-01 16:21:14 +0200

    create data for pizza

--- data/catalog.piza.ts
+++ data/catalog.piza.ts
@@ -3,37 +3,44 @@ interface Ipiza {
   title: string
   image: string
   price: number
+  weight: number
 }
 
-const dataPiza: Ipiza[] = [
+export const dataPiza: Ipiza[] = [
   {
     image: '1.jpg',
     title: 'Піца Гавайська',
-    price: 162
+    price: 162,
+    weight: 280,
   },
   {
     image: '2.jpg',
     title: 'Піца Цезара',
-    price: 186
+    price: 186,
+    weight: 330,
   },
   {
     image: '3.jpg',
     title: 'Піца Пепероні',
-    price: 148
+    price: 148,
+    weight: 120,
   },
   {
     image: '4.jpg',
     title: 'Піца з копченою куркою',
-    price: 205
+    price: 205,
+    weight: 300,
   },
   {
     image: '5.jpg',
     title: 'Піца Д`явола',
-    price: 255
+    price: 255,
+    weight: 350,
   },
   {
     image: '6.jpg',
     title: 'Піца Салямі',
-    price: 110
+    price: 110,
+    weight: 320,
   },
 ]
--- screens/Catalog.tsx
+++ screens/Catalog.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar'
 import { View, Text, StyleSheet, Image, TouchableOpacity  } from 'react-native';
-
+import { dataPiza } from '../data/catalog.piza'
 
 
 const Catalog = () => {
