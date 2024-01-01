813b590343885653b6f436458d522492a3ea3dd7
macs1756 <kindrativmaksum312@gmail.com>
2024-01-01 16:37:01 +0200

    add part styles for my catalog items

--- screens/Catalog.tsx
+++ screens/Catalog.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar'
-import { View, Text, StyleSheet, Image, TouchableOpacity  } from 'react-native';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
 import { dataPiza } from '../data/catalog.piza'
 
 
@@ -9,14 +9,20 @@ const Catalog = () => {
     <View style={styles.container}>
 
       {
-        dataPiza.map((e, i)=>(
+        dataPiza.map((e, i) => (
           <View key={e?.title + i} style={styles.catalogItem}>
-            <Text>{e?.title}</Text>
+
+            <View style={styles.pizzaImageWrapper}>
+              
+            </View>
+
+            <Text style={styles.pizzaWeight}>{e?.weight + ' гр.'}</Text>
+            <Text style={styles.pizzaTitle}>{e?.title}</Text>
           </View>
         ))
       }
 
-      
+
       <StatusBar style="auto" />
     </View>
   );
@@ -38,7 +44,26 @@ const styles = StyleSheet.create({
     width: '100%',
     padding: 10,
     marginBottom: 20
+  },
+
+  pizzaWeight: {
+    fontWeight: '700',
+    color: '#98989d',
+    fontSize: 15,
+    paddingTop: 5,
+    paddingBottom: 5
+  },
+
+  pizzaTitle: {
+    fontSize: 18,
+    fontWeight: '500',
+  },
+
+  pizzaImageWrapper: {
+    width: '100%',
+    height: 150
   }
+
 });
 
 
