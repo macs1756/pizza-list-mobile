25227f77f3c4cc85e067ff34f3e5f5d35df92daf
macs1756 <kindrativmaksum312@gmail.com>
2024-01-01 16:17:04 +0200

    add image

---
+++ data/catalog.piza.ts
@@ -0,0 +1,39 @@
+
+interface Ipiza {
+  title: string
+  image: string
+  price: number
+}
+
+const dataPiza: Ipiza[] = [
+  {
+    image: '1.jpg',
+    title: 'Піца Гавайська',
+    price: 162
+  },
+  {
+    image: '2.jpg',
+    title: 'Піца Цезара',
+    price: 186
+  },
+  {
+    image: '3.jpg',
+    title: 'Піца Пепероні',
+    price: 148
+  },
+  {
+    image: '4.jpg',
+    title: 'Піца з копченою куркою',
+    price: 205
+  },
+  {
+    image: '5.jpg',
+    title: 'Піца Д`явола',
+    price: 255
+  },
+  {
+    image: '6.jpg',
+    title: 'Піца Салямі',
+    price: 110
+  },
+]
